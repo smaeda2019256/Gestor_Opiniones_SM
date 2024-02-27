1c302878485449d28eef1cb32eb44e5c5d902a29
smaeda2019256 <smaeda-2019256@Kinal.edu.gt>
2024-02-26 18:24:14 -0600

    Se crea la clase Server

--- configs/server.js
+++ configs/server.js
@@ -4,5 +4,25 @@ import express from "express";
 import cors from 'cors';
 import helmet from 'helmet';
 import morgan from 'morgan'; 
+import { dbConnection } from "./mongo.js";
 
+class Server {
+    constructor(){
+        this.app = express();
+        this.port = process.env.PORT;
 
+        this.conectarDB();
+    }
+
+    async conectarDB(){
+        await dbConnection();
+    }
+
+    listen(){
+        this.app.listen(this.port, () => {
+            console.log('Server Running on Port: ', this.port);
+        });
+    }
+}
+
+export default Server;
