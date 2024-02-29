1a62076235f6bd43c134fbc874e9a3b1063a5a90
smaeda2019256 <smaeda-2019256@Kinal.edu.gt>
2024-02-28 21:14:06 -0600

    Se crea modelo User

---
+++ src/users/user.model.js
@@ -0,0 +1,30 @@
+import mongoose from "mongoose";
+
+const UserSchema = mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, "El nombre es obligatorio"]
+    },
+    email: {
+        type: String,
+        required: [true, "El correo es obligatorio"],
+        unique: true
+    },
+    password: {
+        type: String,
+        required: [true, "La password es obligatoria"]
+    },
+    estado: {
+        type: Boolean,
+        default: true
+    }
+
+});
+
+UserSchema.methods.toJSON = function() {
+    const {__v, password, _id, ...usuario} = this.toObject();
+    usuario.uid = _id;
+    return usuario;
+}
+
+export default mongoose.model('User', UserSchema);
