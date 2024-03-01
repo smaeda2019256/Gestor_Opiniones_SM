6415fa26f6cfe2351109755142f66c98f951250f
smaeda2019256 <smaeda-2019256@Kinal.edu.gt>
2024-03-01 17:14:14 -0600

    Se crean jwt para los comment

--- src/middlewares/validar-jwt.js
+++ src/middlewares/validar-jwt.js
@@ -6,7 +6,7 @@ export const validarJWT = async(req, res, next) => {
 
     if (!token) {
         return res.status(401).json({
-            msg: "There is no token in the request",
+            msg: "There is NO TOKEN in the request",
         });
     }
 
@@ -15,23 +15,66 @@ export const validarJWT = async(req, res, next) => {
         const usuario = await Usuario.findById(uid);
         if (!usuario) {
             return res.status(401).json({
-                msg: 'User does not exist in the database'
+                msg: 'User does NOT EXIST in the database'
             })
         }
 
         if (!usuario.estado) {
             return res.status(401).json({
-                msg: 'Invalid token - user with status: false'
+                msg: 'INVALID TOKEN - User with status: false'
             })
         }
-
         req.usuario = usuario;
 
         next();
     } catch (e) {
         console.log(e),
             res.status(401).json({
-                msg: "Invalid token",
+                msg: "INVALID TOKEN",
+            });
+    }
+};
+
+export const validarJWTCMNT = async(req, res, next) => {
+    const token = req.header("x-token");
+
+    if (!token) {
+        return res.status(401).json({
+            msg: "There is NO TOKEN in the request",
+        });
+    }
+
+    try {
+        const { uid } = jwt.verify(token, process.env.SECRETORPRIVATEKEY);
+        const usuario = await Usuario.findById(uid);
+        if (!usuario) {
+            return res.status(401).json({
+                msg: 'User does NOT EXIST in the database'
+            })
+        }
+
+        if (!usuario.estado) {
+            return res.status(401).json({
+                msg: 'INVALID TOKEN - User with status: false'
+            })
+        }
+
+        req.usuario = usuario;
+
+        const idPublication = req.params.idPublication;
+        if (!idPublication) {
+            return res.status(400).json({
+                msg: 'Publication ID is MISSING in the request'
+            });
+        }
+
+        req.idPublication = idPublication;
+
+        next();
+    } catch (e) {
+        console.log(e),
+            res.status(401).json({
+                msg: "INVALID TOKEN",
             });
     }
 };
