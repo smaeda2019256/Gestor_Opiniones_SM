b330d9be2fb72e845cde1661fe2ccf5c90438673
smaeda2019256 <smaeda-2019256@Kinal.edu.gt>
2024-03-01 17:02:21 -0600

    Se crean las rutas de comments a trabajar

---
+++ src/comments/comments.routes.js
@@ -0,0 +1,45 @@
+import { Router } from "express";
+import { check } from "express-validator";
+import { validarCampos } from "../middlewares/validarCampos.js";
+import {
+    commentsGet,
+    commentsPost,
+    commentsPut,
+    commentsDelete
+} from "./comments.controller.js";
+import {
+    existeCommentsById,
+} from "../helpers/db-validator.js";
+
+
+const router = Router();
+
+router.get("/", commentsGet);
+
+router.post(
+    "/:idPublication", [
+        check("descriptionComment", "The description is obligatory").not().isEmpty(),
+        validarCampos,
+    ],
+    commentsPost
+);
+
+router.put(
+    "/:id", [
+        check("id", "The ID you entered is NOT VALID").isMongoId(),
+        check("id").custom(existeCommentsById),
+        validarCampos,
+    ],
+    commentsPut
+);
+
+router.delete(
+    "/:id", [
+        check("id", "The ID you entered is NOT VALID").isMongoId(),
+        check("id").custom(existeCommentsById),
+        validarCampos,
+    ],
+    commentsDelete
+);
+
+export default router;
